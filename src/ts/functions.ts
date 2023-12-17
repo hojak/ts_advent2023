c6b8e87a07a32082707d8ea9e562074976aa9397
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-17 13:16:14 +0100

    feat: calibrate single string

--- src/ts/functions.ts
+++ src/ts/functions.ts
@@ -1,3 +1,31 @@
 export function findCalibrationValue (line: string) : number {
-    return 0;
+    let leftDigit = findFirstLeftDigit ( line );
+    let rightDigit = findFirstRightDigit ( line );
+
+    return leftDigit * 10 + rightDigit;
 }
+
+function findFirstLeftDigit ( line: string ) : number {
+    let index = 0;
+    while (index < line.length ) {
+        if ( "0123456789".includes ( line[index] ) ) {
+            return Number (line[index]);
+        }
+        index ++;
+    }
+
+    throw new Error ( "input does not contain digit");    
+}
+
+function findFirstRightDigit ( line: string ) : number {
+    let index = line.length-1;
+    while (index >= 0 ) {
+        if ( "0123456789".includes ( line[index] ) ) {
+            return Number (line[index]);
+        }
+        index --;
+    }
+
+    throw new Error ( "input does not contain digit");    
+}
+
--- test/ts/functions.test.ts
+++ test/ts/functions.test.ts
@@ -4,12 +4,17 @@ import { findCalibrationValue } from "../../src/ts/functions";
 describe ('findCalibrationValue', () => {
 
     [
-        ["0", 0]
+        ["0", 0],
+        ["asdhkasjd1adhuawduiad", 11],
     ].forEach( (inputAndExpected : (string|number)[] ) => {
         it ( inputAndExpected[0] + ' should calibrate to ' + inputAndExpected[0], () => {
             expect(findCalibrationValue (""+inputAndExpected[0])).to.be.deep.equal(inputAndExpected[1]);
         })
     
+    })
+
+    it ( "should throw an error", () => {
+        expect ( () => findCalibrationValue("ahfdahswdoahd")).to.throw(Error, "input does not contain digit");
     })
 
 
