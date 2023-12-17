77a23ad5ab6e4fe9458a1374f639ffbf8e9863d4
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-17 04:58:39 +0100

    refactor: parameterize test

--- test/ts/functions.test.ts
+++ test/ts/functions.test.ts
@@ -2,9 +2,15 @@ import { expect } from "chai";
 import { findCalibrationValue } from "../../src/ts/functions";
 
 describe ('findCalibrationValue', () => {
-    it ('should find 0', () => {
-        expect(findCalibrationValue ('0')).to.be.deep.equal(0);
+
+    [
+        ["0", 0]
+    ].forEach( (inputAndExpected : (string|number)[] ) => {
+        it ( inputAndExpected[0] + ' should calibrate to ' + inputAndExpected[0], () => {
+            expect(findCalibrationValue (""+inputAndExpected[0])).to.be.deep.equal(inputAndExpected[1]);
+        })
+    
     })
 
-    
+
 });
