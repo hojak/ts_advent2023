2db2d65087c2ddfe1248338da31222fa5e24275a
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-17 13:17:52 +0100

    test: add calibration tests for single lines

--- test/ts/functions.test.ts
+++ test/ts/functions.test.ts
@@ -2,10 +2,13 @@ import { expect } from "chai";
 import { findCalibrationValue } from "../../src/ts/functions";
 
 describe ('findCalibrationValue', () => {
-
     [
         ["0", 0],
         ["asdhkasjd1adhuawduiad", 11],
+        ["2sdhkasjdadhuawduiad3", 23],
+        ["asdhka7jdadhuawd9iad", 79],
+        ["23", 23],
+        ["asdhkasjd22", 22],
     ].forEach( (inputAndExpected : (string|number)[] ) => {
         it ( inputAndExpected[0] + ' should calibrate to ' + inputAndExpected[0], () => {
             expect(findCalibrationValue (""+inputAndExpected[0])).to.be.deep.equal(inputAndExpected[1]);
@@ -16,6 +19,4 @@ describe ('findCalibrationValue', () => {
     it ( "should throw an error", () => {
         expect ( () => findCalibrationValue("ahfdahswdoahd")).to.throw(Error, "input does not contain digit");
     })
-
-
 });
