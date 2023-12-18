a5b100ae9e3ddc018802ad3a0c9584575b9628b7
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-18 14:42:20 +0100

    feat: add sum of gear rations to output

--- src/ts/day3/main.ts
+++ src/ts/day3/main.ts
@@ -10,6 +10,9 @@ process.stdin.on ('data', function (part) {
 })
 
 process.stdin.on ('end', function () {
-    console.log ( "compute sum of missing part numbers");
+    console.log ( "compute sum of missing part numbers:");
     console.log ( new Schematic (input).getSumOfMissingParts() );
+
+    console.log ( "compute sum of gear Ratios:");
+    console.log ( new Schematic (input).getSumOfGearRatios() );
 })
