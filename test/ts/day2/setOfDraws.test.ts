9a43e16a139a0c0f5f138a46b54e3d6c02ed4702
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-18 03:26:55 +0100

    refactor: remove unnecessary test

--- test/ts/day2/setOfDraws.test.ts
+++ test/ts/day2/setOfDraws.test.ts
@@ -4,10 +4,6 @@ import { expect } from "chai";
 import { SetOfCubes } from "../../../src/ts/day2/setOfCubes";
 
 describe("SetOfDraws", () => {
-    it ( "should create a set", () => {
-        expect(SetOfDraws.createFromString ("1 red; 1 blue")).not.to.be.null;
-    });
-
     it ( "an empty set should be possible", () => {
         expect(SetOfDraws.createFromString("").isPossibleWith ( new SetOfCubes(1,1,1))).to.be.true;
     });
