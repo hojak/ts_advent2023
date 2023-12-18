ba2f6a34201ac3dd09048bd95c54f3ed5eeb9bf7
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-18 04:00:04 +0100

    feat: add console program

--- src/ts/day2/main.ts
+++ src/ts/day2/main.ts
@@ -11,3 +11,19 @@ export function computeSumOfPossibleGames ( input: string, checkWithSet : string
         .map ( game => game.getNumber())
         .reduce( (prev, current, index ) => prev+current );
 }
+
+
+let input = '';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on ('data', function (part) {
+    input += part
+})
+
+process.stdin.on ('end', function () {
+    console.log ("computing sum of possible games");
+    console.log ( "result: " + computeSumOfPossibleGames ( input, "12 red, 13 green, 14 blue" ));
+})
+
