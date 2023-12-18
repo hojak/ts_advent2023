4313a5877e96a04d7216396fde426ec966a88a48
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-18 03:46:21 +0100

    test: ensure results for game 1 and 3 of the description

--- test/ts/day2/game.test.ts
+++ test/ts/day2/game.test.ts
@@ -24,4 +24,15 @@ describe("Game", () => {
     it ("should have number 12", () => {
         expect (Game.createFromString("Game 12: 2 red").getNumber()).to.be.equal(12);
     })
+
+
+    it ( "Game 1 should be possible", function () {
+        expect ( Game.createFromString("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+            .isPossibleWith(SetOfCubes.createFromString("12 red, 13 green, 14 blue"))).to.be.true;
+    })
+
+    it ( "Game 3 should not be possible", function () {
+        expect ( Game.createFromString("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red")
+            .isPossibleWith(SetOfCubes.createFromString("12 red, 13 green, 14 blue"))).to.be.false;
+    })
 })
