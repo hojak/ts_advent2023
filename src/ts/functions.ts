f5f7651a503f6b40cfe6bd638890dbf0ae5bb1f3
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-18 01:43:37 +0100

    feat: calibrate to all digit names

--- src/ts/functions.ts
+++ src/ts/functions.ts
@@ -1,5 +1,13 @@
 const digitNamesToValues = new Map<string, number>( [
-    ["one", 1]
+    ["one", 1],
+    ["two", 2],
+    ["three", 3],
+    ["four", 4],
+    ["five", 5],
+    ["six", 6],
+    ["seven", 7],
+    ["eight", 8],
+    ["nine", 9],
 ]);
 
 const digitMatcher = new RegExp ("^("+ [ ... digitNamesToValues.keys() ].join("|") + ")", "i");
--- test/ts/functions.test.ts
+++ test/ts/functions.test.ts
@@ -10,7 +10,16 @@ describe ('findCalibrationValue', () => {
         ["23", 23],
         ["asdhkasjd22", 22],
         ["blaonetest1", 11],
-        ["blaonetest", 11]
+        ["blaonetest", 11],
+        ["one", 11],
+        ["two", 22],
+        ["three", 33],
+        ["four", 44],
+        ["five", 55],
+        ["six", 66],
+        ["seven", 77],
+        ["eight", 88],
+        ["nine", 99],
     ].forEach( (inputAndExpected : (string|number)[] ) => {
         it ( inputAndExpected[0] + ' should calibrate to ' + inputAndExpected[0], () => {
             expect(findCalibrationValue (""+inputAndExpected[0])).to.be.deep.equal(inputAndExpected[1]);
@@ -29,7 +38,8 @@ describe ('calibrate', () => {
         ["0", 0],
         ["1", 11],
         ["1\n2", 33],
-        ["1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet", 142]
+        ["1abc2\npqr3stu8vwx\na1b2c3d4e5f\ntreb7uchet", 142],
+        ["two1nine\neightwothree\nabcone2threexyz\nxtwone3four\n4nineeightseven2\nzoneight234\n7pqrstsixteen", 281]
     ].forEach( (inputAndExpected : (string|number)[] ) => {
         it ( inputAndExpected[0] + ' should calibrate to ' + inputAndExpected[0], () => {
             expect(calibrate (""+inputAndExpected[0])).to.be.deep.equal(inputAndExpected[1]);
