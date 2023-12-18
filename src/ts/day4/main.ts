1b9ebe635d47cd27d65c06c65649ff4d869d593c
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-19 00:48:40 +0100

    feat: implement counting cards of part 2

--- src/ts/day4/main.ts
+++ src/ts/day4/main.ts
@@ -9,11 +9,29 @@ export function rateCards ( input : string ) {
 }
 
 
-export function getNumberOfCards( input: string ) {
-    return 0;
+export function getNumberOfCards( input: string ) : number{
+    let cards = input.split("\n").filter(line => line != "")
+        .map ( line => new Card (line));
+
+    let numberOfCards: number[] = new Array (cards.length);
+    for ( let index=0; index < numberOfCards.length; index ++) {
+        numberOfCards[index] = 1;
+    }
+
+    for ( let cardNumber = 0; cardNumber<cards.length; cardNumber++) {
+        let numberOfMatches = cards[cardNumber].getNumberOfMatches ();
+
+        for ( let updateNumber = cardNumber+1; updateNumber<(cardNumber+1+numberOfMatches) && updateNumber<cards.length; updateNumber++) {
+            numberOfCards[updateNumber] += numberOfCards[cardNumber];
+        }
+    }
+
+    return numberOfCards.reduce( (prev, curr, index) => prev+curr);
 }
 
 consoleApp( (input: string) => {
     console.log ( "compute rating of al cards:");
     console.log ( rateCards( input ) );
+    console.log ( "compute number of scratchcards:");
+    console.log ( getNumberOfCards( input ) );
 })
