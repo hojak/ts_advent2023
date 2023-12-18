8f14c6085fb54bc597fed72aedd69c1c33db5fd3
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-18 03:26:15 +0100

    feat: check single draw fails for to small set of cubes

--- src/ts/day2/setOfDraws.ts
+++ src/ts/day2/setOfDraws.ts
@@ -1,13 +1,18 @@
 import { SetOfCubes } from "./setOfCubes";
 
 export class SetOfDraws {
+    draws: SetOfCubes[] = [];
 
     isPossibleWith ( setOfCubes: SetOfCubes ) : boolean {
-        return true;
+        return this.draws
+            .map( set => setOfCubes.contains(set))
+            .reduce((prev, current, index) => prev && current);
     }
 
     static createFromString(representation: string): SetOfDraws {
-        return new SetOfDraws();
+        let result = new SetOfDraws();
+        result.draws.push ( SetOfCubes.createFromString(representation));
+        return result;
     }
 
 }
--- test/ts/day2/setOfDraws.test.ts
+++ test/ts/day2/setOfDraws.test.ts
@@ -11,4 +11,9 @@ describe("SetOfDraws", () => {
     it ( "an empty set should be possible", () => {
         expect(SetOfDraws.createFromString("").isPossibleWith ( new SetOfCubes(1,1,1))).to.be.true;
     });
+
+    it ( "finding 2 reds should not be possible", () => {
+        expect(SetOfDraws.createFromString("2 red").isPossibleWith ( new SetOfCubes(1,1,1))).to.be.false;
+    });
+
 })
