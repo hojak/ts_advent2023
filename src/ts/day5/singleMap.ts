d4cef4028ae253295679720588245b6d0c8e4fce
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-19 01:19:20 +0100

    feat: introduce a single mapping

---
+++ src/ts/day5/singleMap.ts
@@ -0,0 +1,36 @@
+export class SingleMap {
+    destinationStart: number;
+    sourceStart: number;
+    length: number;
+
+    constructor ( sourceStart: number, destinationStart: number, length: number ) {
+        this.sourceStart = sourceStart;
+        this.destinationStart = destinationStart;
+        this.length = length;
+    }
+
+    getSourceStart() : number {
+        return this.sourceStart;
+    }
+
+    map(source: number): number {
+        if ( ! this.isCovered ( source )) {
+            throw new Error("out of bounds");
+        }
+
+        return this.destinationStart + ( source - this.sourceStart);
+    }
+
+    isCovered(source: number): boolean {
+        return source >= this.sourceStart && source <= this.sourceStart + this.length-1
+    }
+
+    static createFromString(representation: string): any {
+        let splitted = representation.split(" ");
+        return new SingleMap ( 
+            Number(splitted[0].trim()),
+            Number(splitted[1].trim()),
+            Number(splitted[2].trim())
+        )
+    }
+}
---
+++ test/ts/day5/singleMap.test.ts
@@ -0,0 +1,41 @@
+import { expect } from "chai";
+import { describe } from "mocha";
+import { SingleMap } from "../../../src/ts/day5/singleMap";
+
+describe("day 5", () => {
+    
+    describe ("singleMap", () => {
+        let testee = new SingleMap ( 1, 10, 5 );
+        it ( "should be the correct start", () => {
+            expect( testee.getSourceStart()).to.be.equal(1);
+        });
+
+        [
+            [1, 10],
+            [5, 14]
+        ].forEach((expectedMapping: number[]) => {
+            it ( expectedMapping[0] + " should map to " + expectedMapping[1], () => {
+                expect( testee.map(expectedMapping[0])).to.be.equal(expectedMapping[1]);
+            })
+        });
+
+
+        [0, -1, 6, 10, 100].forEach ( (source : number) => {
+            it ( source + " should not be covered", () => {
+                expect ( testee.isCovered ( source )).to.be.false;
+            })
+        });
+
+        [1, 2, 3, 4, 5].forEach ( (source : number) => {
+            it ( source + " should be covered", () => {
+                expect ( testee.isCovered ( source )).to.be.true;
+            })
+        })
+    })
+
+    describe ("singleMap.createFromString", () => {
+        it ( "should create a correct map", () => {
+            expect ( SingleMap.createFromString("1 2 3")).to.be.deep.equal ( new SingleMap (1, 2, 3));
+        })
+    })
+});
