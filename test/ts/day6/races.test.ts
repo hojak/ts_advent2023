bb5e09d2a0cd289f5331bfe1b9971e6d9aa245e6
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-20 02:43:15 +0100

    feat: compute number of winning possibilities

---
+++ test/ts/day6/races.test.ts
@@ -0,0 +1,21 @@
+import { expect } from "chai";
+import { describe } from "mocha";
+import { Races } from "../../../src/ts/day6/races";
+
+describe ( "Day 6: Races", () => {
+    let testee = new Races ( 
+        "Time:      7  15   30\n"+
+        "Distance:  9  40  200"
+    );
+
+    it ( "should be 4 for the first race", () => {
+        expect( testee.getWinningPossibilitiesOfRace(0)).to.be.equal(4);
+    });
+    it ( "should be 8 for the 2nd race", () => {
+        expect( testee.getWinningPossibilitiesOfRace(1)).to.be.equal(8);
+    });
+    it ( "should be 9 for the 3rd race", () => {
+        expect( testee.getWinningPossibilitiesOfRace(2)).to.be.equal(9);
+    });
+
+})
