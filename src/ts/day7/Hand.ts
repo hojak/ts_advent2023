c2689eb854e155410b951b3448d08c736aefd627
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-20 15:24:30 +0100

    feat: determine all hand types

--- src/ts/day7/Hand.ts
+++ src/ts/day7/Hand.ts
@@ -1,12 +1,53 @@
 export class Hand {
     cards: string;
+    cardCount: { [card: string]: number } = {};
+    type: HandType = HandType.HighCard;
 
     constructor ( cards: string ) {
         this.cards = cards;
+
+        this.countCards(cards);
+
+        this.determineCardType();
+    }
+    
+    determineCardType() {
+        let numberOfDifferentCards = Object.keys(this.cardCount).length;
+        let counts = Object.values ( this.cardCount ).sort ( (a,b) => b-a);
+
+        switch ( numberOfDifferentCards ) {
+            case 5: this.type = HandType.HighCard; break;
+            case 1: this.type = HandType.FiveOfAKind; break;
+            case 4: this.type = HandType.OnePair; break;
+            case 2:
+                if ( counts[0] == 3) {
+                    this.type = HandType.FullHouse;
+                } else {
+                    this.type = HandType.FourOfAKind;
+                }
+                break;
+            case 3:
+                if ( (counts[0]) == 3) {
+                    this.type = HandType.ThreeOfAKind;
+                } else {
+                    this.type = HandType.TwoPairs;
+                }
+                break;
+        }
+    }
+
+    private countCards(cards: string) {
+        cards.split("").forEach(card => {
+            if (!(card in this.cardCount)) {
+                this.cardCount[card] = 1;
+            } else {
+                this.cardCount[card]++;
+            }
+        });
     }
 
     getType () : HandType {
-        return HandType.FiveOfAKind
+        return this.type;
     }
 }
 
--- test/ts/day7/hand.test.ts
+++ test/ts/day7/hand.test.ts
@@ -6,5 +6,33 @@ describe ("day 7: hand", () => {
         it ("should be of type five of a kind", () => {
             expect ( new Hand ("AAAAA").getType()).to.be.equal ( HandType.FiveOfAKind);
         })
+
+        it ("should be of type four of a kind", () => {
+            expect ( new Hand ("AAAAK").getType()).to.be.equal ( HandType.FourOfAKind);
+        })
+
+        it ("should be of type full house", () => {
+            expect ( new Hand ("12121").getType()).to.be.equal ( HandType.FullHouse);
+        })
+
+        it ("should be of type three of a kind", () => {
+            expect ( new Hand ("B2B3B").getType()).to.be.equal ( HandType.ThreeOfAKind);
+        })
+
+        it ("should be of type two pairs", () => {
+            expect ( new Hand ("11A22").getType()).to.be.equal ( HandType.TwoPairs);
+        })
+
+        it ("should be of type one pair", () => {
+            expect ( new Hand ("TA23T").getType()).to.be.equal ( HandType.OnePair);
+        })
+
+        it ("should be of type High Card", () => {
+            expect ( new Hand ("BJT23").getType()).to.be.equal ( HandType.HighCard);
+        })
+
+
+
+
     } )
 });
