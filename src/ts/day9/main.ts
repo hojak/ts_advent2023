4a50b7515777c03db03a1041b9b49fb46d608b92
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-21 04:40:26 +0100

    feat: implement main app for day 9

---
+++ src/ts/day9/main.ts
@@ -0,0 +1,7 @@
+import { consoleApp } from "../consoleApp";
+import { sum_of_next_steps } from "./next_step";
+
+consoleApp (input => {
+    console.log ( "getting sum of next steps: ");
+    console.log ( sum_of_next_steps(input));
+})
