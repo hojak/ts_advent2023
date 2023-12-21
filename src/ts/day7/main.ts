8ce2e7a5f178aa91f9c66d94e5315fb2002d2d4f
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-21 01:47:37 +0100

    feat: return winnings with joker in main program

--- src/ts/day7/main.ts
+++ src/ts/day7/main.ts
@@ -2,7 +2,9 @@ import { consoleApp } from "../consoleApp";
 import { ListOfHands } from "./listOfHands";
 
 consoleApp (input => {
-    console.log ( "comput winnigns for input: ");
+    console.log ( "compute winnigns for input: ");
     console.log ( new ListOfHands(input).getTotalWinnings());
 
+    console.log ( "use J as joker: ");
+    console.log ( new ListOfHands(input, true).getTotalWinnings());
 })
