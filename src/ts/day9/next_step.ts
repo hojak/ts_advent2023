6b4d1c2ceb1e4bbaef6e348d96fa6178bc992056
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-21 04:35:34 +0100

    feat: compute sum of next steps for test input

---
+++ src/ts/day9/next_step.ts
@@ -0,0 +1,26 @@
+import { transpileModule } from "typescript";
+
+export function next_step ( sequence : number[] ): number {
+    if ( sequence.length == 0 ) {
+        return 0;
+    } else if ( sequence.length == 1 ) {
+        return sequence[0];
+    }
+
+    let diffs : number[] = [];
+    for ( let i=1; i<sequence.length; i++ ) {
+        diffs.push(sequence[i]-sequence[i-1]);
+    }
+
+    return sequence[sequence.length-1] + next_step(diffs);
+}
+
+
+export function sum_of_next_steps ( input: string ) : number {
+    let sum = 0;
+
+    return input.split("\n").filter(line => line.trim() >= "")
+        .map ( line => line.split(" ").filter ( part => part.trim() != "").map ( part => Number(part)))
+        .map ( sequence => next_step(sequence ))
+        .reduce ( (prev, curr, index) => prev+curr);
+}
