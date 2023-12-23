4ff42b6f4596eb3105dbc661db8ea2667465126d
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-23 04:05:07 +0100

    refactor: use new getLines to implement other line getters

--- src/ts/day13/Area.ts
+++ src/ts/day13/Area.ts
@@ -25,24 +25,11 @@ export class Area {
     }
 
     getFirstLines(numberOfLines: number): string[] {
-        let result: string[] = [];
-
-        for ( let line = 0; line < numberOfLines; line ++) {
-            result[line] = this.lines[line];
-        }
-
-        return result;
+        return this.getLines(0, numberOfLines-1);
     }
 
     getLastLines(numberOfLines: number): string[] {
-        let result: string[] = [];
-
-        const startingWithLine = this.lines.length - numberOfLines;
-        for ( let line = startingWithLine; line < this.lines.length; line ++) {
-            result[line-startingWithLine] = this.lines[line];
-        }
-
-        return result;
+        return this.getLines( this.lines.length - numberOfLines, this.lines.length-1);
     }
 
     getLines(startWithLine: number, endWithLine: number): string[] {
