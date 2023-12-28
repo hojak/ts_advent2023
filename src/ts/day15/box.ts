6a70e21e9f86fdb1d77c8ba28a219965982db01b
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-28 13:00:22 +0100

    feat: add a new lens to a box

--- src/ts/day15/box.ts
+++ src/ts/day15/box.ts
@@ -1,9 +1,20 @@
 export class Box {
 
+    private _lenses: Lens[] = [];
 
-    sumOfFocusingPower(): number {
-        return 0;
+    addLens(label: string, focalLength: number) : this {
+        this._lenses.push ( { label: label, focalLength: focalLength } );
+        return this;
     }
 
 
+    sumOfFocusingPower(): number {
+        return this._lenses.reduce ( (prev, curr, index) => prev + curr.focalLength * (index+1), 0 );
+    }
+
+}
+
+interface Lens {
+    label: string;
+    focalLength: number;
 }
--- test/ts/day15/box.test.ts
+++ test/ts/day15/box.test.ts
@@ -5,11 +5,15 @@ import { Box } from "../../../src/ts/day15/box";
 describe ( "Day 15: Box", () => {
 
     describe ( "sumOfFocusingPower", () => {
-        it ( "shoult return 0 for an empty box", () => {
+        it ( "should return 0 for an empty box", () => {
             expect( new Box ().sumOfFocusingPower()).to.be.equal(0)
         })
-    });
 
+        it ( "should return 5 after adding a  lens with 5 as focal length", () => {
+            expect ( new Box().addLens ( "aa", 5).sumOfFocusingPower()).to.be.equal (5);
+        })
+
+    });
 
 });
 
