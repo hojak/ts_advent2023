d9692ecf5ad04a91f400837e6a72101763beff5a
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-28 15:02:41 +0100

    feat: add evaqluating initualization to main app

--- src/ts/day15/main.ts
+++ src/ts/day15/main.ts
@@ -1,7 +1,11 @@
 import { consoleApp } from "../consoleApp";
 import { checksum } from "./hash";
+import { WallOfBoxes } from "./wallOfBoxes";
 
 consoleApp (input => {
     console.log ( "computing checksum");
     console.log ( checksum(input));
+
+    console.log ( "sum of lens power after initialization")
+    console.log ( new WallOfBoxes( input ).evaluateInitialization());
 })
