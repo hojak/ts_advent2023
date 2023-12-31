79ec92093d35a6c7248e5ff6c1bf970c2162a06a
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-31 13:17:58 +0100

    feat: add decoding plan to main app

--- src/ts/day18/main.ts
+++ src/ts/day18/main.ts
@@ -1,7 +1,18 @@
 import { consoleApp } from "../consoleApp"
 import { GroundPlan } from "./groundPlan";
+import { transformPlan } from "./transformPlan";
 
 consoleApp (input => {
     console.log ( "let's start digging");
-    console.log ( new GroundPlan().digAsPlanned(input).digOutInterior().numberOfDiggedSquares() );
+    const groundPlan = new GroundPlan().digAsPlanned(input);
+    groundPlan.printBoundaries();
+    console.log ( groundPlan.digOutInterior().numberOfDiggedSquares() );
+
+
+    console.log ( "decode plan");
+    let decoded = transformPlan(input);
+
+    const groundPlanDecoded = new GroundPlan().digAsPlanned(decoded);
+    groundPlanDecoded.printBoundaries();
+    console.log ( groundPlanDecoded.digOutInterior().numberOfDiggedSquares() );
 })
