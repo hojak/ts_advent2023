504427945ddc940ac3172c4060731f03edd7a44b
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2023-12-31 14:54:52 +0100

    refactor: transform part into class

--- src/ts/day19/part.ts
+++ src/ts/day19/part.ts
@@ -1,8 +1,34 @@
-export interface Part {
-    x: number,
-    m: number,
-    a: number,
-    s: number
+import { isThisTypeNode } from "typescript";
+
+export class Part {
+    private _x: number;
+    private _m: number;
+    private _a: number;
+    private _s: number;
+
+    constructor ( x: number, m: number, a: number, s: number) {
+        this._x = x;
+        this._m = m;
+        this._a = a;
+        this._s = s;
+    }
+
+    public get x(): number {
+        return this._x;
+    }
+    public get m(): number {
+        return this._m;
+    }
+    public get a(): number {
+        return this._a;
+    }
+    public get s(): number {
+        return this._s;
+    }
+
+    public getSum () : number {
+        return this._x + this._m + this._a + this._s;
+    }
 }
 
 export function createPartFromString (description: string): Part {
@@ -13,10 +39,5 @@ export function createPartFromString (description: string): Part {
         throw new Error ( description + " is not a part description!");
     }
 
-    return {
-        x: Number(groups[1]),
-        m: Number(groups[2]),
-        a: Number(groups[3]),
-        s: Number(groups[4])
-    }
+    return new Part (Number(groups[1]),Number(groups[2]),Number(groups[3]),Number(groups[4]));
 }
--- test/ts/day19/workflow.test.ts
+++ test/ts/day19/workflow.test.ts
@@ -6,18 +6,18 @@ import { Part } from "../../../src/ts/day19/part";
 describe ( "Day 19: Workflow", () => {
     describe ("execute", () => {
         it ( "should return accepted", () => {
-            expect(new Workflow("aaa{A}").perform( { x: 0, m: 0, a: 0, s: 0 })).to.be.equals("A");
+            expect(new Workflow("aaa{A}").perform( new Part ( 0, 0, 0, 0 ))).to.be.equals("A");
         })
 
         let testee = new Workflow ( "px{a<2006:qkq,m>2090:A,rfg}");
         it ( "should accept a large m", () => {
-            expect( testee.perform ( {x: 0, a: 3000, m:3000, s: 0})).to.be.equal("A");
+            expect( testee.perform ( new Part ( 0,3000,3000, 0))).to.be.equal("A");
         })
         it ( "should route small a to qkq", () => {
-            expect( testee.perform ( {x: 0, a: 1000, m:3000, s: 0})).to.be.equal("qkq");
+            expect( testee.perform ( new Part ( 0,3000, 2000, 0))).to.be.equal("qkq");
         })
         it ( "should route to rfg otherwise", () => {
-            expect( testee.perform ( {x: 0, a: 3000, m:2000, s: 0})).to.be.equal("rfg");
+            expect( testee.perform ( new Part (0, 2000, 3000, 0))).to.be.equal("rfg");
         })
     });
 });
