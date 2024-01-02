167578a90423b1e4709d87d56e6bd7748f560579
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-01-02 02:28:29 +0100

    feat: add walk in infinite garden to main app

--- src/ts/day21/main.ts
+++ src/ts/day21/main.ts
@@ -8,4 +8,7 @@ consoleApp (input => {
     let garden = new Garden ( input );
     console.log ( "I could reach " + garden.getNumberOfReachablePots ( 64) + " different pots!");
     
+
+    console.log ( "lets see, where a really long walk will can take us");
+    console.log ( "I could reach " + garden.getNumberOfReachablePots ( 26501365 ) + " different pots!");
 })
