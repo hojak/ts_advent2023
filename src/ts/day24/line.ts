5e9f9b596c9c03f8e207f098bc64a70a11972d02
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-01-03 19:20:18 +0100

    feat: find crossing in the critical area

--- src/ts/day24/line.ts
+++ src/ts/day24/line.ts
@@ -23,16 +23,46 @@ export class Line {
             return new Vector(NaN, NaN, NaN);
         }
 
-        let thisFactor = (that.startPosition.x - this.startPosition.x) / this._direction.x
-            + that._direction.x / this._direction.x 
-                * (this._direction.x*(that._startPosition.y - this._startPosition.y)-this._direction.y*(that.startPosition.x - this.startPosition.x))
-                / (that.direction.x*this.direction.y - that.direction.y*this.direction.x)
+        let thisFactor = this.getCrossingFactor(that)
 
         return new Vector ( 
             Math.round((this.startPosition.x + this._direction.x * thisFactor) * 1000) / 1000,
             Math.round((this.startPosition.y + this._direction.y * thisFactor) * 1000) / 1000,
             0
         );                
+    }
+
+
+    private getCrossingFactor(that: Line) :number {
+        return (that.startPosition.x - this.startPosition.x) / this._direction.x
+            + that._direction.x / this._direction.x
+            * (this._direction.x * (that._startPosition.y - this._startPosition.y) - this._direction.y * (that.startPosition.x - this.startPosition.x))
+            / (that.direction.x * this.direction.y - that.direction.y * this.direction.x);
+    }
+
+    hasCriticalCrossing(thatLine: Line, topLeft: Vector, bottomRight: Vector): boolean {
+        if ( this._direction.isParallelInXY( thatLine._direction )) {
+            return false;
+        }
+
+        let thisFactor = this.getCrossingFactor(thatLine);
+        if ( thisFactor < 0 ) {
+            return false;
+        }
+
+        let thatFactor = thatLine.getCrossingFactor(this);
+        if ( thatFactor < 0 ) {
+            return false;
+        }
+        
+        let crossingPoint = new Vector ( 
+            Math.round((this.startPosition.x + this._direction.x * thisFactor) * 1000) / 1000,
+            Math.round((this.startPosition.y + this._direction.y * thisFactor) * 1000) / 1000,
+            0
+        );  
+
+        return topLeft.x <= crossingPoint.x && topLeft.y <= crossingPoint.y 
+            && bottomRight.x >= crossingPoint.x && bottomRight.y >= crossingPoint.y;
     }
 
 }
--- test/ts/day24/line.test.ts
+++ test/ts/day24/line.test.ts
@@ -25,6 +25,30 @@ describe ("Day 24: Line", () => {
                     .getCrossingPointXY ( new Line ( "20, 25, 34 @ -2, -2, -4"))
                     ).to.be.deep.equal ( new Vector (11.667, 16.667, 0));
         });
+    });
 
-    })
+    describe ( "crossInArea", () => {
+        [
+            ["19, 13, 30 @ -2, 1, -2", "18, 19, 22 @ -1, -1, -2"],
+            ["19, 13, 30 @ -2, 1, -2", "20, 25, 34 @ -2, -2, -4"]
+        ].forEach ( pair => {
+            it ( "should find a critical crossing for pair " + pair, () => {
+                expect ( new Line( pair[0]).hasCriticalCrossing( new Line(pair[1]), new Vector(7,7,0), new Vector(27,27,0))).to.be.true;
+            })
+        });
+
+        [
+            ["19, 13, 30 @ -2, 1, -2", "20, 19, 15 @ 1, -5, -3"],
+            ["18, 19, 22 @ -1, -1, -2", "20, 25, 34 @ -2, -2, -4"],
+            ["18, 19, 22 @ -1, -1, -2", "12, 31, 28 @ -1, -2, -1"],
+            ["18, 19, 22 @ -1, -1, -2", "20, 19, 15 @ 1, -5, -3"],
+            ["20, 25, 34 @ -2, -2, -4", "20, 19, 15 @ 1, -5, -3"],
+            ["19, 13, 30 @ -2, 1, -2", "12, 31, 28 @ -1, -2, -1"]
+        ].forEach ( pair => {
+            it ( "should not find a critical crossing for pair " + pair, () => {
+                expect ( new Line( pair[0]).hasCriticalCrossing( new Line(pair[1]), new Vector(7,7,0), new Vector(27,27,0))).to.be.false;
+            })
+        });
+
+    });
 });
