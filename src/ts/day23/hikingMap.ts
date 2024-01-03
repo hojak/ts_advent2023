319a59c3a4d969b5f4af0610254f5fb62479063c
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-01-03 10:22:59 +0100

    feat: find the longest walk for the test input

--- src/ts/day23/hikingMap.ts
+++ src/ts/day23/hikingMap.ts
@@ -4,9 +4,9 @@ export class HikingMap {
     private _height: number;
     
     constructor ( map: string ) {
-        this._map = map.replace(/\n/g, "");
+        this._map = map;
         this._width = map.indexOf("\n");
-        this._height = map.length - this._map.length + 1;
+        this._height = map.length - map.replace(/\n/g, "").length + 1;
     }
     public get width(): number {
         return this._width;
@@ -14,4 +14,115 @@ export class HikingMap {
     public get height(): number {
         return this._height;
     }
+
+    getLongestWalk(): string {
+        const startPosition = this.getStartPosition();
+        return this.markPositionOnMap (this.getStartPosition(), "S")
+            .markPositionOnMap (this.getFinishPosition(), "F")
+            ._getLongestWalk(startPosition);
+    }   
+
+    getFinishPosition(): Position {
+        return {x: this.width-2, y: this.height-1 };
+    }
+
+    private _getLongestWalk(currentPosition: Position): string {
+        const directions = [
+            {x: 0, y: 1},
+            {x: 0, y: -1},
+            {x: 1, y: 0},
+            {x: -1, y: 0},
+        ]
+
+        let currentLongestLength = -1;
+        let currentLongestWalk = "";
+
+        let checkedDirections = 0;
+
+        for ( let direction of directions ) {
+            const posInDirection = addPositions(currentPosition, direction);
+            let tileInDirection = this.getSymbolAt(posInDirection);
+
+            let nextWalk : HikingMap | undefined = undefined;
+            let startOfNextWalk = posInDirection;
+
+            if ( tileInDirection == ".") {
+                nextWalk = this.markPositionOnMap(posInDirection, "O");
+            } else if ( tileInDirection == ">") {
+                if ( direction.x != -1 ) {
+                    startOfNextWalk = addPositions ( posInDirection, {x: 1, y:0})
+                    nextWalk = this.markPositionOnMap(posInDirection, "O")
+                        .markPositionOnMap(startOfNextWalk, "O");
+                }
+            } else if ( tileInDirection == "<") {
+                if ( direction.x != 1 ) {
+                    startOfNextWalk = addPositions ( posInDirection, {x: -1, y:0})
+                    nextWalk = this.markPositionOnMap(posInDirection, "O")
+                        .markPositionOnMap(startOfNextWalk, "O");
+                }
+            } else if ( tileInDirection == "^" ) {
+                if ( direction.y != 1) {
+                    startOfNextWalk = addPositions ( posInDirection, {x: 0, y:-1})
+                    nextWalk = this.markPositionOnMap(posInDirection, "O")
+                        .markPositionOnMap(startOfNextWalk, "O");
+                }
+            } else if ( tileInDirection == "v") {
+                if ( direction.y != -1 ) {
+                    startOfNextWalk = addPositions ( posInDirection, {x: 0, y:1})
+                    nextWalk = this.markPositionOnMap(posInDirection, "O")
+                        .markPositionOnMap(startOfNextWalk, "O");
+                }
+            } else if ( tileInDirection == "F") {
+                return this._map;
+            }
+
+            if ( nextWalk != undefined ) {
+                let walkForPosition = nextWalk._getLongestWalk(startOfNextWalk);
+                let lengthOfWalkForPosition = lengthOfWalk(walkForPosition);
+                checkedDirections++;
+
+                if (lengthOfWalkForPosition > currentLongestLength ) {
+                    currentLongestLength = lengthOfWalkForPosition;
+                    currentLongestWalk = walkForPosition;
+                }
+            }
+        }
+
+        return currentLongestWalk;
+    }
+
+    getSymbolAt(position: Position) {
+        return this._map[this._stringPositionFor( position )];
+    }
+
+    markPositionOnMap(position: Position, symbol: string): HikingMap {
+        return new HikingMap( 
+            this._map.substring ( 0, this._stringPositionFor(position) )
+            + symbol
+            +  this._map.substring ( this._stringPositionFor(position)+1 ) 
+        );
+    }
+
+    private _stringPositionFor(position: Position) {
+        return position.x + (this._width+1)*position.y;
+    }
+
+    getStartPosition(): Position {
+        return { x: 1, y: 0};
+    }
 }
+
+
+interface Position {
+    x: number,
+    y: number
+}
+
+function addPositions(pos1: Position, pos2: Position): Position {
+    return {x: pos1.x+pos2.x, y: pos1.y+pos2.y};
+}
+
+function lengthOfWalk(walkForPosition: string) {
+    return walkForPosition.length - walkForPosition.replace(/O/g,"").length +1 ;
+}
+
