e2f55c35acb5c7459ab974a80c62f800d9fc75a4
Holger Janßen-Kroll <git@ho-jk.de>
2024-01-04 06:26:42 +0100

    feat: identify cycle in 1 billion iterations and compute result based on the cycle

--- src/ts/day14/main.ts
+++ src/ts/day14/main.ts
@@ -5,14 +5,34 @@ consoleApp (input => {
     console.log ( "getting weight of tilted platform ");
     console.log ( new Platform(input).tilt().getTotalLoad () );
 
-    console.log ( "ok, let's do the million!");
+    console.log ( "ok, let's do the billion!");
+
+    let found : Map<string, number> = new Map(); 
+    let numbers : Map<number, number> = new Map();
 
     let platform = new Platform(input);
-    for ( let i=0; i<1000000; i++ ) {
-        if ( i % 10000 == 0) {
-            console.log ( "We're at " + Intl.NumberFormat('de-de').format ( i));
+    for ( let i=0; i<1000000000; i++ ) {
+        if ( i % 1000000 == 0) {
+            console.log ( new Date() + ": We're at " + Intl.NumberFormat('de-de').format ( i));
         }
-        platform.cylce();
+        platform.cycle();
+        
+        let str = platform.toString();
+        if ( found.has ( str )) {
+            console.log ( new Date() + ": found cycle at iteration " + i);
+            let cycleOffset = found.get(str) ?? 0;
+            let cycleLength = i-cycleOffset;
+
+            let targetIteration =  cycleOffset + (1000000000 - i -1) % cycleLength;
+            let result = numbers.get(targetIteration) ?? -1;
+            
+            console.log ( "result: " + result );
+            return;
+        } else {
+            found.set(str, i);
+            numbers.set(i, platform.getTotalLoad());
+        }
     }
     console.log ( "resulting load: " + platform.getTotalLoad());
+    console.log ( platform.toString() );
 })
