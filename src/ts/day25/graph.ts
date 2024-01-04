75b73d856e833cf87ad8dc32b49a3c794c086a7c
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-01-04 14:25:54 +0100

    redesign: implement graph a adjacent matrix
    partitioning algorithm is much slower

--- src/ts/day25/graph.ts
+++ src/ts/day25/graph.ts
@@ -1,69 +1,81 @@
-import { GraphNode } from "./graphNode";
-
 export class Graph {
-    private _nodes: Map<string, GraphNode> = new Map();
+    private _nodes: string[] = [];
+    private _matrix: boolean[][] = [];
 
     constructor ( description: string ) {
-        description.split("\n")
-            .map( line => line.trim()).filter ( line => line != "")
-            .forEach (line => this.addLine ( line ))
+        this._initializeNodes(description);
+        this._initializeMatrix(description);
+    }
+
+    private _initializeMatrix(description: string) {
+        for ( let i=0; i < this._nodes.length; i ++) {
+            this._matrix[i] = [];
+            for ( let j=0; j < this._nodes.length; j ++) {
+                this._matrix[i][j] = false;
+            }
+        }
+
+        for ( let line of description.split("\n")) {
+            let [name, connectedNodes] = line.split(":");
+
+            for ( let node of connectedNodes.trim().split(/ +/)) {
+                this.addEdgeBetweenNodeNames(name, node);
+            }
+        }
+    }
+    
+    private _initializeNodes(description: string) {
+        let setOfNodeNames : Set<string> = new Set();
+        for ( let line of description.split("\n")) {
+            let [name, connectedNodes] = line.split(":");
+            setOfNodeNames.add ( name.trim() );
+
+            for ( let node of connectedNodes.trim().split(/ +/)) {
+                setOfNodeNames.add ( node.trim());
+            }
+        }
+        this._nodes = Array.from(setOfNodeNames.values()).sort();
     }
 
     public get numberOfNodes(): number {
-        return this._nodes.size;
+        return this._nodes.length;
     }
 
     public get numberOfEdges(): number {
-        return this.getListOfEdgeDescriptions().length;
-    }
-
-
-    addLine(line: string): void {
-        let split = line.split(":");
-        let leftNodeName = split[0].trim();
-        
-        for ( let nodeName of split[1].trim().split(/ +/)) {
-            this.addEdgeBetweenNodeNames ( leftNodeName, nodeName );
-        }
-    }
-
-    addEdge(leftNode: GraphNode, rightNode: GraphNode) {
-        leftNode.addEdge ( rightNode );
-        rightNode.addEdge ( leftNode );
+        return this.getListOfEdges().length;
     }
 
     addEdgeBetweenNodeNames ( leftNodeName: string, rightNodeName: string ) {
-        this.addEdge( this.getOrCreateNode(leftNodeName), this.getOrCreateNode(rightNodeName));
+        let index1 = this.findIndexForNodeName(leftNodeName);
+        let index2 = this.findIndexForNodeName(rightNodeName);
+
+        this.addEdgeBetweenNodeIds(index1, index2);
     }
 
-
-    getOrCreateNode(name: string) : GraphNode {
-        let result = this._nodes.get(name);
-        if ( result == undefined ) {
-            let newNode = new GraphNode(name);
-            this._nodes.set ( name, newNode );
-            return newNode;
-        } else {
-            return result;
-        }
+    private addEdgeBetweenNodeIds(index1: number, index2: number) {
+        this._matrix[index1][index2] = true;
+        this._matrix[index2][index1] = true;
     }
 
-    getPartitionSize(nodeName: string): number {
-        let node = this._nodes.get(nodeName);
-        if ( node == undefined ) {
+    private findIndexForNodeName(name: string) {
+        return this._nodes.findIndex(element => element == name);
+    }
+
+    getPartitionSize(nodeIndex: number): number {
+        if ( nodeIndex == undefined ) {
             return 0;
         }
 
-        let reachableNodes : Set<GraphNode> = new Set ();
-        let queue = [node];
+        let reachableNodes : Set<number> = new Set ();
+        let queue = [nodeIndex];
         while ( queue.length > 0 ) {
-            node = queue.pop();
-            if ( node == undefined ) {
+            let nodeIndex = queue.pop();
+            if ( nodeIndex == undefined ) {
                 continue;
             }
 
-            reachableNodes.add(node);
-            for ( let connectedNode of node.connectedNodes ) {
+            reachableNodes.add(nodeIndex);
+            for ( let connectedNode of this.getConnectedNodes(nodeIndex) ) {
                 if ( ! reachableNodes.has(connectedNode)) {
                     queue.push( connectedNode );
                 }
@@ -73,64 +85,77 @@ export class Graph {
         return reachableNodes.size;
     }
 
-    getListOfEdgeDescriptions(): string[] {
-        let result: string[] = [];
-        for ( let node of this._nodes.values() ) {
-            for ( let connectedTo of node.connectedNodes) {
-                if ( node.name < connectedTo.name) {
-                    result.push ( node.name + "," + connectedTo.name);
+    getConnectedNodes(nodeIndex: number) {
+        let result: number[] = []
+        for ( let i=0; i<this._matrix.length; i++) {
+            if ( this._matrix[nodeIndex][i]) {
+                result.push(i);
+            }
+        }
+        return result;
+    }
+
+
+    getListOfEdges(): number[][] {
+        let result: number[][] = [];
+        for ( let node1=0; node1<this._matrix.length-1; node1++ ) {
+            for ( let node2= node1+1; node2<this._matrix.length; node2++) {
+                if ( this.edgeExists(node1, node2)) {
+                    result.push ( [node1, node2] );
                 }
             }
         }
-        return result.sort();
+        return result;
+    }
+
+    edgeExists(node1: number, node2: number) {
+        return this._matrix[node1][node2];
     }
 
 
     findPartitioningByRemovingEdges(): number[] {
-        let edges = this.getListOfEdgeDescriptions();
-        let sizeOfGraph = this._nodes.size;
+        let edges = this.getListOfEdges();
 
         let counter = 0;
 
         for ( let index1 = 0; index1 < edges.length-2; index1++) {
-            let [node11, node12] = edges[index1].split(",");
-            this.removeEdge ( node11, node12);
+            let [node11, node12] = edges[index1];
+            this.removeEdgeById ( node11, node12);
             for ( let index2 = index1+1; index2 < edges.length-1; index2++) {
-                let [node21, node22] = edges[index2].split(",");
-                this.removeEdge ( node21, node22);
+                let [node21, node22] = edges[index2]
+                this.removeEdgeById ( node21, node22);
                 for ( let index3 = index2+1; index3 < edges.length; index3++) {
                     counter ++;
                     if ( counter % 1000 == 0) {
                         console.log ( new Date() + ": number of checked edge combinations: " + counter)
                     }
 
-                    let [node31, node32] = edges[index3].split(",");
-                    this.removeEdge ( node31, node32);
+                    let [node31, node32] = edges[index3]
+                    this.removeEdgeById ( node31, node32);
 
-                    let sizeOfPartition = this.getPartitionSize(this._nodes.keys().next().value );
-                    if ( sizeOfPartition < sizeOfGraph ) {
-                        return [sizeOfPartition, sizeOfGraph-sizeOfPartition].sort();
+                    let sizeOfPartition = this.getPartitionSize(0);
+                    if ( sizeOfPartition < this._nodes.length ) {
+                        return [sizeOfPartition, this._nodes.length-sizeOfPartition].sort();
                     }
-                    this.addEdgeBetweenNodeNames(node31, node32);                    
+                    this.addEdgeBetweenNodeIds(node31, node32);                    
                 }
-                this.addEdgeBetweenNodeNames(node21, node22);
+                this.addEdgeBetweenNodeIds(node21, node22);
             }
-            this.addEdgeBetweenNodeNames(node11, node12);
+            this.addEdgeBetweenNodeIds(Number(node11), Number(node12));
         }
 
-        return [0, this._nodes.size];
+        return [0, this._nodes.length];
+    }
+
+    removeEdgeById(nodeId1: number, nodeId2: number) {
+        this._matrix[nodeId1][nodeId2] = false;
+        this._matrix[nodeId2][nodeId1] = false;
     }
 
     removeEdge(nodeName1: string, nodeName2: string) {
-        let node1 = this._nodes.get(nodeName1);
-        let node2 = this._nodes.get(nodeName2);
-
-        if  ( node1 == undefined || node2 == undefined ) {
-            return;
-        }
-
-        node1.removeEdge(node2);
-        node2.removeEdge(node1);
+        let nodeId1 = this.findIndexForNodeName(nodeName1);
+        let nodeId2 = this.findIndexForNodeName(nodeName2);
+        this.removeEdgeById(nodeId1, nodeId2);
     }
 
 
--- test/ts/day25/graph.test.ts
+++ test/ts/day25/graph.test.ts
@@ -5,16 +5,16 @@ import { Graph } from "../../../src/ts/day25/graph";
 describe( "Day 25: Graph", () => {
     describe ( "getPartitionSize", () => {
         it ( "should return 2 for a graph with two connected nodes", () => {
-            expect(new Graph("a: b").getPartitionSize("a")).to.be.equal(2);
+            expect(new Graph("a: b").getPartitionSize(0)).to.be.equal(2);
         })
     } )
 
     describe ( "getListOfEdges", () => {
         it ( "should return a list with x edges", () => {
-            expect(new Graph("a: b\nc: d\ne: a").getListOfEdgeDescriptions()).to.be.deep.equal([
-                "a,b",
-                "a,e",
-                "c,d"
+            expect(new Graph("a: b\nc: d\ne: a").getListOfEdges()).to.be.deep.equal([
+                [0,1],
+                [0,4],
+                [2,3]
             ]);
         })
     } )
