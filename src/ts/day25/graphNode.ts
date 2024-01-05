d190b2f6b3d53309cda90f153e4a5a6e10a1b5c8
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-01-05 01:14:23 +0100

    redesign: graph can store weights in the edges

--- src/ts/day25/graphNode.ts
+++ src/ts/day25/graphNode.ts
@@ -1,24 +1,33 @@
 export class GraphNode {
-    
     private _name: string;
-    private _connectedTo: Set<GraphNode> = new Set();
+    private _edges: Set<Edge> = new Set();
     
     constructor ( name: string ) {
         this._name = name;
     }
 
-    addEdge(thatNode: GraphNode) {
-        this._connectedTo.add (thatNode);
+    addEdge(thatNode: GraphNode, weight: number = 1) {
+        this._edges.add ({ 
+            node: thatNode, weight: weight
+        });
     }
     removeEdge(thatNode: GraphNode) {
-        this._connectedTo.delete(thatNode);
+        for ( let edge of this._edges) {
+            if ( edge.node.name == thatNode.name) {
+                this._edges.delete ( edge );
+            }
+        }
     }
 
-    public get connectedNodes(): Set<GraphNode> {
-        return this._connectedTo;
+    public get connectedNodes(): GraphNode[] {
+        return Array.from(this._edges.values()).map(edge => edge.node);
     }
     public get name(): string {
         return this._name;
     }
+}
 
+interface Edge {
+    node: GraphNode,
+    weight: number
 }
