4d00a58490a4abf2c6777382caca9063395b8925
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-01-24 18:01:04 +0100

    test: test behavior on a network with a simple loop

--- test/ts/day20/moduleConfiguration.test.ts
+++ test/ts/day20/moduleConfiguration.test.ts
@@ -77,4 +77,77 @@ describe("Day 20: ModuleConfiguration", () => {
             expect (testee.getNumberOfProcessedSignals()).to.be.deep.equal ([3,0]);
         });
     });
+
+
+    describe("simple loop computation", () => {
+
+        /**
+         *    push  1 | 2  | 3 | 4  | 5  | 6 | 7  | 8
+         * 
+         *     bc
+         *     |
+         *     |    l | l  | l | l  | l  | l | l  | ....
+         *     v
+         *  F->%a
+         *  |  |    
+         *  |  |    h | l. | h | lh | l. | h | lh | l
+         *  |  v        ^^^^^^^^^^^^ LOOP
+         *  |  %b
+         *  |  |    
+         *  L--J    . | h  | . | l. | h  | . | l. | h
+         *                   ^^^^^^^^^^^^  LOOP
+         * 
+         */
+        let testee = new ModuleConfiguration(
+            "broadcaster -> a\n"+
+            "%a -> b\n"+
+            "%b -> a"
+        );
+
+        testee.pushTheButton(8);
+
+        it ( "should have reached a loop incoming at b", () => {
+            expect(testee.getModule("b")?.received.signals).to.be.deep.equal ([
+                {push: 1, sender: "a", receiver: "b", type: SignalType.High},
+
+                {push: 2, sender: "a", receiver: "b", type: SignalType.Low},
+                {push: 3, sender: "a", receiver: "b", type: SignalType.High},
+                {push: 4, sender: "a", receiver: "b", type: SignalType.Low},
+                {push: 4, sender: "a", receiver: "b", type: SignalType.High},
+
+                {push: 5, sender: "a", receiver: "b", type: SignalType.Low},
+                {push: 6, sender: "a", receiver: "b", type: SignalType.High},
+                {push: 7, sender: "a", receiver: "b", type: SignalType.Low},
+                {push: 7, sender: "a", receiver: "b", type: SignalType.High},
+
+                {push: 8, sender: "a", receiver: "b", type: SignalType.Low},
+            ]);
+        })
+
+        it ( "at a loop back input should be combined with broadcaster signal", () => {
+            expect(testee.getModule("a")?.received.signals).to.be.deep.equal ([
+                {push: 1, sender: "broadcaster", receiver: "a", type: SignalType.Low},
+
+                {push: 2, sender: "broadcaster", receiver: "a", type: SignalType.Low},
+                {push: 2, sender: "b", receiver: "a", type: SignalType.High},
+
+                {push: 3, sender: "broadcaster", receiver: "a", type: SignalType.Low},
+
+                {push: 4, sender: "broadcaster", receiver: "a", type: SignalType.Low},
+                {push: 4, sender: "b", receiver: "a", type: SignalType.Low},
+
+                {push: 5, sender: "broadcaster", receiver: "a", type: SignalType.Low},
+                {push: 5, sender: "b", receiver: "a", type: SignalType.High},
+
+                {push: 6, sender: "broadcaster", receiver: "a", type: SignalType.Low},
+
+                {push: 7, sender: "broadcaster", receiver: "a", type: SignalType.Low},
+                {push: 7, sender: "b", receiver: "a", type: SignalType.Low},
+
+                {push: 8, sender: "broadcaster", receiver: "a", type: SignalType.Low},
+                {push: 8, sender: "b", receiver: "a", type: SignalType.High},
+            ]);
+
+        });
+    })
 });
