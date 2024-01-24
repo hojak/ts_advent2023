9efaf6cc61a8aa868a550d7b2e8c83c00f6204f7
Holger Janßen-Kroll <hjanssenkroll@igus.net>
2024-01-24 18:00:21 +0100

    feat: prepare deviding sequence in intial and loop

--- src/ts/day20/signalSequence.ts
+++ src/ts/day20/signalSequence.ts
@@ -1,26 +1,63 @@
 import { Signal, SignalType } from "./signal";
 
 export class SignalSequence {
-    private _sequence: Signal[] = [];
+    private _initialSequence: Signal[] = [];
+
+    private _signalLoop: Signal[];
+
+    constructor ( initialSignals: Signal[] = [], signalLoop : Signal[] = [] ) {
+        this._initialSequence = initialSignals;
+        this._signalLoop = signalLoop;
+    }
 
     add ( signal: Signal ) {
-        this._sequence.push(signal);
+        this._initialSequence.push(signal);
     }
 
     public get signals () {
-        return this._sequence;
+        return this._initialSequence;
     }
 
     public get numberOfLowSignals () : number {
-        return this._sequence.filter ( signal => signal.type == SignalType.Low ).length;
+        return this._initialSequence.filter ( signal => signal.type == SignalType.Low ).length;
     }
 
     public get numberOfHighSignals () : number {
-        return this._sequence.filter ( signal => signal.type == SignalType.High ).length;
+        return this._initialSequence.filter ( signal => signal.type == SignalType.High ).length;
     }
 
-    getSignalsReceivedInPush(push: number): Signal[] {
-        return this._sequence.filter ( signal => signal.push == push );
+    public get numberOfPushesInInitialSequence () : number {
+        if ( this._initialSequence.length == 0) {
+            return 0;
+        } else {
+            return this._initialSequence[this._initialSequence.length-1].push;
+        }
     }
 
+    getSignalsOfPush(push: number): Signal[] {
+        if ( push <= this.numberOfPushesInInitialSequence ) {
+            return this._initialSequence.filter( signal => signal.push == push);
+        }
+
+        if ( this._signalLoop.length == 0 ) {
+            return [];
+        }
+
+        let modulo = (push-this.numberOfPushesInInitialSequence) % this.numberOfPushesInLoop;
+        let offset = push - modulo;
+
+        return this._signalLoop
+            .filter( signal => signal.push == modulo)
+            .map( signal => { return {
+                type: signal.type,
+                sender: signal.sender,
+                receiver: signal.receiver,
+                push: signal.push + offset
+            }});
+    }
+
+
+    private get numberOfPushesInLoop() {
+        return this._signalLoop[this._signalLoop.length - 1].push;
+    }
 }
--- test/ts/day20/signalSequence.test.ts
+++ test/ts/day20/signalSequence.test.ts
@@ -13,9 +13,55 @@ describe ( "Day 20: SignalSequence", () => {
         testee.add ( {sender: "a", receiver: "b", type: SignalType.Low, push: 4});
 
         it ( "should return both signals of push 2", () => {
-        expect ( testee.getSignalsReceivedInPush(2)).to.be.deep.equal ( [
+        expect ( testee.getSignalsOfPush(2)).to.be.deep.equal ( [
             {sender: "a", receiver: "b", type: SignalType.Low, push: 2},
             {sender: "a", receiver: "b", type: SignalType.Low, push: 2}
         ])});
     });
+
+
+    describe ("simple loops", () => {
+        let testee = new SignalSequence ([], [
+            {sender: "a", receiver: "b", type: SignalType.Low, push: 1},
+            {sender: "c", receiver: "b", type: SignalType.High, push: 1},
+            {sender: "a", receiver: "b", type: SignalType.Low, push: 2}
+        ]);
+
+        it ( "should return the 2 signals of the first push of the loop", () => {
+            expect ( testee.getSignalsOfPush(11)).to.be.deep.equal ( [
+                {sender: "a", receiver: "b", type: SignalType.Low, push: 11},
+                {sender: "c", receiver: "b", type: SignalType.High, push: 11}
+            ]);
+        });
+    });
+
+    describe ("simple loop with initial sequence", () => {
+        let testee = new SignalSequence ([
+            {sender: "a", receiver: "b", type: SignalType.Low, push: 1},
+            {sender: "a", receiver: "b", type: SignalType.Low, push: 2},
+            {sender: "a", receiver: "b", type: SignalType.Low, push: 2}
+        ],[
+            {sender: "c", receiver: "b", type: SignalType.Low, push: 1},
+            {sender: "c", receiver: "b", type: SignalType.High, push: 3},
+        ]);
+
+        it ( "should return the correct signal of the initial sequence", () => {
+            expect ( testee.getSignalsOfPush(2)).to.be.deep.equal ( [
+                {sender: "a", receiver: "b", type: SignalType.Low, push: 2},
+                {sender: "a", receiver: "b", type: SignalType.Low, push: 2}
+            ]);
+        });
+
+        it ( "should return no signal for the middle of the loop", () => {
+            expect ( testee.getSignalsOfPush(7)).to.be.empty;
+        });
+
+        it ( "should return the signal of the first push of the loop", () => {
+            expect ( testee.getSignalsOfPush(6)).to.be.deep.equal([
+                {sender: "c", receiver: "b", type: SignalType.Low, push: 6},
+            ]);
+        });
+    });
 });
+
+
